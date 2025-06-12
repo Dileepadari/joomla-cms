3a86a6e0fb35379b72c4e3d1ba17a90234dba4aa
Dileepadari <adaridileep@gmail.com>
2025-06-12 13:24:29 +0530

    menu options added in the UI

--- administrator/components/com_workflow/resources/scripts/components/app.vue
+++ administrator/components/com_workflow/resources/scripts/components/app.vue
@@ -1,3 +1,27 @@
 <template>
-  <div class="container" >For testing</div>
+  <div class="workflow-graph-container container-fluid vh-100 d-flex flex-column">
+    <div class="workflow-graph-menu-bar row border-bottom shadow-sm py-3 px-4">
+      <WorkflowToolbar />
+    </div>
+    <div class="workflow-graph-inner-container row flex-grow-1">
+      <!-- Workflow graph canvas goes here -->
+    </div>
+  </div>
 </template>
+
+<script>
+import WorkflowToolbar from './toolbar/toolbar.vue'
+
+export default {
+  name: 'WorkflowGraphApp',
+  components: {
+    WorkflowToolbar
+  },
+  data() {
+    return {
+      // The full height of the app in px
+      fullHeight: '',
+    }
+  }
+}
+</script>
